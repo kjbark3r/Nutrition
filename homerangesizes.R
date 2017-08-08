5f95a4ce496d706263bd18f7ed052ad941a2807a
MCWRU01-LW17\kristin <kristin.barker@umconnect.umt.edu>
2017-08-07 18:25:38 -0600

    updated this one file to include new work laptop working directory and filepaths

--- homerangesizes.R
+++ homerangesizes.R
@@ -9,16 +9,15 @@
 
 wd_workcomp <- "C:\\Users\\kristin.barker\\Documents\\GitHub\\Nutrition"
 wd_laptop <- "C:\\Users\\kjbark3r\\Documents\\GitHub\\Nutrition"
-if (file.exists(wd_workcomp)) {
-  setwd(wd_workcomp)
+wd_worklaptop <- "C:\\Users\\kristin\\Documents\\Nutrition"
+if (file.exists(wd_workcomp)) {setwd(wd_workcomp)
 } else {
-  if(file.exists(wd_laptop)) {
-    setwd(wd_laptop)
+  if(file.exists(wd_laptop)) {setwd(wd_laptop)
   } else {
-    cat("Are you SURE you got that file path right?\n")
+    setwd(wd_worklaptop)
   }
 }
-rm(wd_workcomp, wd_laptop)
+rm(wd_workcomp, wd_laptop, wd_worklaptop)
 
 # PACKAGES
 
@@ -32,8 +31,8 @@ memory.limit(size = 7500000)
 #### DATA & SETUP ####
 
 
-# read & prep elk locations (from Access DB, processed in ElkDatabase/dataprep.R)
-locs <- read.csv("../ElkDatabase/collardata-locsonly-equalsampling.csv") %>%
+# read & prep elk locations (from Access DB, processed in DatabasesEtc/dataprep.R)
+locs <- read.csv("../DatabasesEtc/collardata-locsonly-equalsampling.csv") %>%
   dplyr::select(c(AnimalID, Date, Time, Lat, Long, Sex)) %>%
   within(Date <- as.Date(Date, format = "%Y-%m-%d")) %>% #format date
   filter(Sex == "Female") %>% #not using males for nutrition analysis
@@ -56,19 +55,19 @@ stpln <- spTransform(ll, stateplane)
 kud <- kernelUD(stpln[,7]) #create kde for each indiv (7=IndivYr)
 hrs <- getverticeshr(kud, unin = "m", unout = "km2") #calculate kde areas
 hr.a <- as.data.frame(hrs) %>%
-  rename(IndivYr = id, HRarea = area)
+  rename(IndivYr = id, SumArea = area)
 
 write.csv(hr.a, file = "homerangeareas.csv", row.names = FALSE)
 sumraster <- raster(kud)
 writeRaster(sumraster, paste("summerkdes"), format="GTiff", overwrite=TRUE)
 
 ####  REDO FOR WINTER TO MAKE PRESN MAPS ####
-# read & prep elk locations (from Access DB, processed in ElkDatabase/dataprep.R)
-winlocs <- read.csv("../ElkDatabase/collardata-locsonly-equalsampling.csv") %>%
+# read & prep elk locations (from Access DB, processed in DatabasesEtc/dataprep.R)
+winlocs <- read.csv("../DatabasesEtc/collardata-locsonly-equalsampling.csv") %>%
   dplyr::select(c(AnimalID, Date, Time, Lat, Long, Sex)) %>%
   within(Date <- as.Date(Date, format = "%Y-%m-%d")) %>% #format date
   filter(Sex == "Female") %>% #not using males for nutrition analysis
-  subset(between(Date, as.Date("2014-02-26"), as.Date("2014-03-31")) | #summer
+  subset(between(Date, as.Date("2014-02-26"), as.Date("2014-03-31")) | #winter
          between(Date, as.Date("2015-02-15"), as.Date("2015-03-31"))) %>% 
   mutate(IndivYr = ifelse(Date < "2015-01-01", 	 # add indiv id (elk-year)
 						   paste(AnimalID, "-14", sep=""),
@@ -77,17 +76,31 @@ xy <- data.frame("x" = winlocs$Long, "y" = winlocs$Lat)
 ll <- SpatialPointsDataFrame(xy, winlocs, proj4string = latlong)
 stpln <- spTransform(ll, stateplane)
 winkud <- kernelUD(stpln[,7]) #create kde for each indiv (7=IndivYr)
-winhrs <- getverticeshr(winkud)
+winhrs <- getverticeshr(winkud, unin = "m", unout = "km2")
+hr.b <- as.data.frame(winhrs) %>%
+  rename(IndivYr = id, WinArea = area)
 
 winraster <- raster(winhrs)
 writeRaster(winraster, paste("winterkdes"), format="GTiff", overwrite=TRUE)
 
 
+
+#### Combine summer and winter HR data ####
+# and check whether intermediates may be "expanders"
+allhrs <- hr.a %>%
+  full_join(hr.b, by = "IndivYr") %>%
+  mutate(DiffArea = SumArea - WinArea ) 
+summary(allhrs$DiffArea)
+mig <- read.csv("migstatus.csv")
+hrmig <- full_join(allhrs, mig, by = "IndivYr")
+intmig <- filter(hrmig, MigStatus == "Intermediate")
+summary(intmig$DiffArea) # nope, more like contracters
+
 # sanity check to make sure they look reasonable
 # with predicted de raster from Vegetation/de_model.R as background
 de14 <- raster("../Vegetation/DE2014.tif")
 plot(de14) 
-plot(hrs, add = T)
+plot(hrs, add = T, col = "red")
 plot(winhrs, add = T, col = "blue")
 
 
